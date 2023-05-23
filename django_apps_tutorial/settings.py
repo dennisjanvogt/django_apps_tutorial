dfdf2142628b6bbacea4ef9232828ad8b9de4093
dennisjanvogt <dennisjanvogt@gmail.com>
2023-05-23 18:08:39 +0200

    Initial Commit

--- django_apps_tutorial/settings.py
+++ django_apps_tutorial/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "main.apps.MainConfig",
+    "versicherung.apps.VersicherungConfig",
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "de"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Berlin"
 
 USE_I18N = True
 
