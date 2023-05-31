d97b2d08673c16192963639d2cfee5748ed2611a
dennisjanvogt <dennisjanvogt@gmail.com>
2023-05-31 18:31:57 +0200

    added forms

---
+++ versicherung/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from .models import Kunde, Mitarbeiter, Schadensfall, Versicherungsvertrag
+
+
+class MitarbeiterForm(forms.ModelForm):
+    class Meta:
+        model = Mitarbeiter
+        field = "__all__"
+        widgets = {
+            "geburtsdatum": forms.DateInput(attrs={"type": "date"}),
+            "einstellungsdatum": forms.DateInput(attrs={"type": "date"}),
+        }
+
+
+class SchadensfallForm(forms.ModelForm):
+    versicherungsvertrag = forms.ModelChoiceField(
+        queryset=Versicherungsvertrag.objects.all(), empty_label="(Keine Auswahl)"
+    )
+
+    class Meta:
+        model = Schadensfall
+        fields = "__all__"
+
+
+class VersicherungsvertragForm(forms.ModelForm):
+    kunde = forms.ModelChoiceField(
+        queryset=Kunde.objects.all(), empty_label="(Keine Auswahl)"
+    )
+    mitarbeiter = forms.ModelChoiceField(
+        queryset=Mitarbeiter.objects.all(), empty_label="(Keine Auswahl)"
+    )
+
+    class Meta:
+        model = Versicherungsvertrag
+        fields = "__all__"
+        widgets = {
+            "startdatum": forms.DateInput(attrs={"type": "date"}),
+            "enddatum": forms.DateInput(attrs={"type": "date"}),
+        }
+
+
+class KundeForm(forms.ModelForm):
+    class Meta:
+        model = Kunde
+        fields = "__all__"
